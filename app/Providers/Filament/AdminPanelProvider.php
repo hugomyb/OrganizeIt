c9d202b3de2df997e045a3b31b1957cca74a439e
Mayonobe Hugo <hugomayonobe@gmail.com>
2024-06-21 12:03:21 +0200

    add favicon

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -31,6 +31,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('/')
+            ->favicon(asset('/img/organize-it.png'))
             ->login()
             ->colors([
                 'primary' => Color::Blue,
