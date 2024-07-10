f98b08dbca160ed50c72da3d7603703b3816658e
Mayonobe Hugo <hugomayonobe@gmail.com>
2024-07-10 16:59:29 +0200

    [+] add keybinding for searchbar

--- resources/views/components/searchbar.blade.php
+++ resources/views/components/searchbar.blade.php
@@ -1,6 +1,8 @@
 <x-filament::input.wrapper
     prefix-icon="heroicon-m-magnifying-glass"
     prefix-icon-alias="panels::global-search.field"
+    suffix="Ctrl+K"
+    suffix-color="gray"
     inline-prefix
     inline-suffix
     wire:target="search"
@@ -9,6 +11,7 @@
     <x-filament::input
         autocomplete="off"
         inline-prefix
+        id="taskSearchbar"
         :placeholder="__('general.search_task_placeholder')"
         type="search"
         wire:key="search.field.input"
@@ -17,6 +20,7 @@
                 \Filament\Support\prepare_inherited_attributes(
                     new \Illuminate\View\ComponentAttributeBag([
                         'wire:model.live' => 'search',
+                        'x-mousetrap.command-k.ctrl-k' => 'document.getElementById(\'taskSearchbar\').focus()'
                     ])
                 )
             "
