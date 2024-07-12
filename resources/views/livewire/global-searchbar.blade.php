02af1ce6f831d99ff6301dce6a8f477d8710f591
Mayonobe Hugo <hugomayonobe@gmail.com>
2024-07-12 11:37:11 +0200

    [FIX] add keyboard navigation to global searchbar

--- resources/views/livewire/global-searchbar.blade.php
+++ resources/views/livewire/global-searchbar.blade.php
@@ -113,7 +113,7 @@
                      style="scrollbar-color: #4F46E5 #E5E7EB; overflow-y: auto; max-height: 50vh; scrollbar-width: thin">
                     <ul class="flex flex-col gap-2 rounded-lg">
                         <template x-for="(result, index) in results" :key="index">
-                            <li class="rounded-lg w-full bg-gray-100 dark:bg-gray-800 transition ease-in-out focus:!bg-blue-100 hover:bg-blue-100 dark:hover:!bg-blue-100 dark:hover:!text-black"
+                            <li class="rounded-lg !outline-none w-full bg-gray-100 dark:bg-gray-800 transition ease-in-out focus:!bg-blue-100 hover:bg-blue-100 dark:hover:!bg-blue-100 dark:hover:!text-black"
                                 tabindex="-1" x-bind:id="'result-' + index">
                                 <a :href="result.url"
                                    class="rounded-lg w-full">
